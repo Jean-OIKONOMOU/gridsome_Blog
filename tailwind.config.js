f831d511ea34aa29d697a904550275997df88f59
Jean-OIKONOMOU <jean.oikonomou@gmail.com>
2020-10-11 21:03:51 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    colors: {
+      primary: '#00BFA6',
+      primarydark: '#2C7A7B',
+      blogbg: '#E2E8F0',
+      background: '#F7FAFC',
+      white: '#FFF',
+      darkgray: '#2D3748',
+      lightgray: '#A0AEC0'
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
